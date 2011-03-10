15da607a43dc8a4e48836e88a1730c14f9d34e59
Gaurav Midha <gaurav.midha@vinsol.com>
2011-03-10 16:16:04 +0530

    Dynamic finders exercise with 2 modules for diffrent methods

---
+++ dynamic_finders2.rb
@@ -0,0 +1,89 @@
+module M 
+  @@db = {}
+  @@index = 0
+
+  def self.included(cls)
+    cls.extend ClassMethods
+    cls.send :class_variable_set, "@@db", {}
+    cls.send :class_variable_set, "@@index", 0
+  end
+
+  def save()    
+
+    key1 = self.instance_variables  
+    key1.each do |key|
+      key = /\w+/.match(key)
+      @@db[key.to_s] ||= {}
+    end
+    if validate()    
+      @@db.each_key { |key| @@db[key][@@index.to_s] = self.send key }
+      @@index = @@index + 1
+    end
+  end
+  def validate
+    var = self.instance_variables 
+    test = true
+    var.each do |var1|
+      var1 = /\w+/.match(var1)
+      if (self.send var1.to_s ).length > 20
+        puts "length of #{var1} should be less than 20 characters"
+        test = false
+      end
+    end
+    return test
+  end
+  module ClassMethods
+      def method_missing(m, args)
+        @@db = self.send :class_variable_get, "@@db"
+        @@index = self.send :class_variable_get, "@@index"
+        puts "heloojkkj"
+        key = nil
+        if m.to_s.start_with?("find_by_")
+          temp = /(?<=find_by_).+/.match(m)
+          temp = temp.to_s
+          if @@db.has_key?(temp) 
+            define_singleton_method("#{m}") do |args|
+              if @@db[temp].has_value?(args)
+                @@db[temp].each do |indx,value|             
+                  key = indx    if(value == args)
+                end  
+              end  #if
+              @@db.each_key{ |indx| print "#{indx}=>#{@@db[indx][key]}  " }
+              print "\n"
+            end #define_method
+            #puts self.instance_methods.sort
+            self.send "#{m}", args
+          else
+            puts "#{args} not found"  
+          end
+        else
+          super
+        end #if
+      end #method_missing
+  end #ClassMethods
+end #class
+class Play
+  attr_accessor :fname,:lname
+  def self.print_db
+    puts @@db
+  end
+include M
+end
+p1 = Play.new
+p1.fname = "gaurav"
+p1.save
+#puts Play.print_db
+p2 = Play.new
+p2.fname = "khushi"
+p2.lname = "yadav"
+p2.save
+#puts Play.print_db
+p3 = Play.new
+p3.fname = "vilok"
+p3.lname = "aggarwalsjj"
+p3.validate
+p3.save
+#puts Play.print_db
+Play.find_by_fname("gaurav")
+Play.find_by_fname("khushi")
+
